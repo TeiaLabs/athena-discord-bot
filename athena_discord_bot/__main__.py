7628d39916c6d1eb71da2d84c2a4ba016eb73605
RamiroNB <ramiro.nilson.barros@gmail.com>
2022-11-16 15:40:46 -0300

    Add reactions input

--- athena_discord_bot/__main__.py
+++ athena_discord_bot/__main__.py
@@ -7,7 +7,9 @@ from .athena_client import AthenaClient
 
 dotenv.load_dotenv()
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents = intents)
 ATHENA_DISCORD_BOT_TOKEN = os.getenv("ATHENA_DISCORD_BOT_TOKEN")
 if ATHENA_DISCORD_BOT_TOKEN is None:
     raise ValueError("Failed to load env variables.")
@@ -27,5 +29,11 @@ async def on_message(message):
     print(body)
     await message.channel.send(body["response_text"])
 
+@client.event
+async def on_reaction_add(reaction, user):
+    if reaction.message.author == client.user:
+        print("Client:", reaction)
+        print("Client:", user)
+        AthenaClient().chat(reaction.message)
 
 client.run(ATHENA_DISCORD_BOT_TOKEN)
