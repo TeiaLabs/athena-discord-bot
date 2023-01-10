249478242e0908757be3ff9e532e1fd7ed196b88
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-01-10 15:22:04 -0300

    Add thread implementation to AthenaBot

--- athena_discord_bot/__main__.py
+++ athena_discord_bot/__main__.py
@@ -1,6 +1,7 @@
 import dotenv
 import os
 
+from discord import Message, DMChannel, Thread
 import discord
 
 from .athena_client import AthenaClient
@@ -9,7 +10,7 @@ dotenv.load_dotenv()
 
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents = intents)
+client = discord.Client(intents=intents)
 ATHENA_DISCORD_BOT_TOKEN = os.getenv("ATHENA_DISCORD_BOT_TOKEN")
 if ATHENA_DISCORD_BOT_TOKEN is None:
     raise ValueError("Failed to load env variables.")
@@ -21,13 +22,21 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: Message):
     print("Discord:", repr(message))
     if message.author == client.user or message.content == "":
         return
     body = AthenaClient().chat(message)
     print(body)
-    await message.reply(body["response_text"])
+
+    if isinstance(message.channel, Thread) or isinstance(message.channel, DMChannel):
+        await message.reply(body["response_text"])
+    else:
+        thread = await message.create_thread(
+            name=f"{message.author.name}:{message.content}",
+        )
+        await thread.send(body["response_text"])
+
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -36,8 +45,9 @@ async def on_reaction_add(reaction, user):
     print("Client:", reaction)
     print("Client:", user)
     AthenaClient().chat(reaction.message)
-    if reaction.emoji == '📸'or reaction.emoji == '📷':
+    if reaction.emoji == "📸" or reaction.emoji == "📷":
         body = AthenaClient().poet_url(reaction.message.reference.message_id)
         await reaction.message.channel.send(body)
+
 
 client.run(ATHENA_DISCORD_BOT_TOKEN)
